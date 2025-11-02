afe693f93c27b6478a67a0cba08cc220f994dc3e
shravan jain <shravanjain3065@gmail.com>
2025-11-02 09:19:42 +0530

    Ignore .class files

--- Shravan_jain_java/java-basics/basics.java
+++ Shravan_jain_java/java-basics/basics.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.util.Scanner;
+import javax.sound.sampled.SourceDataLine;
 
 public class basics {
 
@@ -11,6 +12,22 @@ public class basics {
         System.out.println("Enter the number:");
         int val = sc.nextInt();
         System.out.println("You Entered:" + val);
+        //data types and variables
+        int age = 72;
+        System.out.println("The age of the person is:" + age);
+        float height = 184.5f;
+        System.out.println("The height of the person is" + height);
+        double pi_value = 3.1422882828;
+        System.out.println("The value of the pi:" + pi_value);
+        char hold_character = 'h';
+        System.out.println("The character:" + hold_character);
+        boolean b1 = true;
+        boolean b2 = false;
+        // if else condition
+        if (age < 18) System.out.println("Not eligible to drive");
+        else System.out.println("Eligible to drive");
+        // for loop
+        for (int i = 0; i < 10; i++) System.out.println(i);
         sc.close();
     }
 }
